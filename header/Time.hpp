fb434bc5c4ac2f9dcc4840c23f55eb279c620a6b
Ay <inuhama1543@gmail.com>
2024-10-30 22:21:52 +0900

    update

--- header/Time.hpp
+++ header/Time.hpp
@@ -1,5 +1,50 @@
+/*
+	@file Time.hpp
+	@brief 時間処理関係
+	@author Ay
+	@date 2024/10/30
+*/
 #pragma once
+#include "../header/Utility.hpp"
+#include "../header/Concepts.hpp"
 
 namespace Ay {
-
+	/*
+	@class Time
+	@brief 時間処理関係
+*/
+	class Time {
+	private:
+		float timeScale;   // タイムスケール
+		steady_clock::time_point deltaTimer; // 時刻保存用
+	public:
+		/*
+			@brief コンストラクタ
+		*/
+		Time();
+		/*
+			@brief デフォルトデストラクタ
+		*/
+		~Time() = default;
+		/*
+			@brief デルタタイムを取得
+			@return 経過時間
+		*/
+		float DeltaTime();
+		/*
+			@brief 全体の時間スケールをセット
+			@param scale セットする値
+		*/
+		void SetTimeScale(float scale) {
+			timeScale = scale;
+		}
+		/*
+			@brief 指定時間処理を止める
+			@param sec 処理を止める秒数 (浮動小数点型のみ)
+		*/
+		template <Floating T>
+		static void WaitForSeconds(T sec) {
+			sleep_for(duration<T>(sec));
+		}
+	};
 }
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,1 +1,15 @@
 #include "../header/Time.hpp"
+
+namespace Ay {
+	Time::Time()
+		: timeScale(1.f)
+		, deltaTimer(high_resolution_clock::now()) {
+	}
+
+	float Time::DeltaTime() {
+		auto now = high_resolution_clock::now(); // 現時刻を取得
+		duration<float> t = now - deltaTimer;    // 経過時間を計算
+		deltaTimer = now;                                     // 更新
+		return t.count() * timeScale;
+	}
+}
